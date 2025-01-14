c70598569e1bc2830e5762cca14b07267d5fc102
ChaseBugeaud <cbugeaud@shaw.ca>
2025-01-14 14:05:28 -0800

    added regex checking

--- lab1/calculate.js
+++ lab1/calculate.js
@@ -1,6 +1,9 @@
 
 function getValue() {
   let binaryValue = document.getElementById("binary-input").value;
+  if (!/^[01]{1,32}$/.test(binaryValue)) {
+    alert("must be a binary number up to 32 bits long");
+  }
   outputDecimal(binaryValue);
 }
 
